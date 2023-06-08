336f95d4f759c58fd8f0d8acbb1c0cde38b605bc
Elenpet2014 <elenpet2014@gmail.com>
2023-06-08 09:43:02 +0400

    add fails

---
+++ java_lesson/Hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+	public static void main(String[] args) {
+        String firstName = "Elen";
+        String lastName = "Petrosyan";
+        
+        
+		System.out.println(firstName+" "+lastName);
+		
+	}
+}
