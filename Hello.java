f20d4a50cdf40ea7fa64abc9f94eba4d92909924
Elenpet2014 <elenpet2014@gmail.com>
2023-06-13 11:16:45 +0400

    Homework 2

---
+++ Hello.java
@@ -0,0 +1,16 @@
+public class Hello{
+	public static void main(String[] args) {
+        String firstName = "Elen";
+        String lastName = "Petrosyan";
+        
+        
+		System.out.println(firstName+" "+lastName);
+		System.out.println("Hello Tatev");
+
+		int a = 5;
+		int b = 6;
+		System.out.println(a + b);
+		System.out.println(a * b);
+		System.out.println(a - b);
+	}
+}
