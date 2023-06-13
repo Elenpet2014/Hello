0b230b6243420e8c0b549140efdf2d892ea5d1d9
Elenpet2014 <elenpet2014@gmail.com>
2023-06-13 11:13:26 +0400

    Homework

---
+++ Homework.java
@@ -0,0 +1,20 @@
+public class Homework{
+	public static void main(String[] args) {
+		int [] ages = {30, 30, 33, 31, 40, 41};
+		String [] names = new String[6];
+		names[0] = "Anahit";
+		names[1] = "Tatev";
+		names[2] = "Irina";
+		names[3] = "Gagik";
+		names[4] = "Elen";
+		names[5] = "Anna";
+
+		String [] name = {"Anahit", "Tatev", "Irina", "Gagik", "Elen", "Anna"};
+
+		
+		
+		System.out.println("Name is " + names[4] + ", age is " + ages[4] );
+
+		System.out.println("Name is " + name[5] + ", age is " + ages[5] );
+	}
+}
